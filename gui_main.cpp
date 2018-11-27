075e0fc9c8ba8fe9a80052798f94cbbb8644f2d2
Farseen CK <itsfarseen@gmail.com>
2018-11-27 17:08:51 +0530

    Added context menu for boards and entries.

--- gui_main.cpp
+++ gui_main.cpp
@@ -20,7 +20,19 @@ struct Item {
 struct Board {
     std::string title;
     std::vector<Item> items;
+
+    //////////////////////////////////////
+    // UI Stuff 
+    //////////////////////////////////////
+
     size_t editing_index = SIZE_MAX;
+    // Which item is being edited
+
+    size_t hovered_index = SIZE_MAX;
+    // which item is being hovered
+
+    std::string title_new;
+    // Used as a buffer while renaming the board
 };
 
 std::vector<Board> BOARDS; 
@@ -46,25 +58,120 @@ void gui_setup(int argc, char **argv) {
 }
 
 static void render_board(size_t index, Board *board);
-static void render_board_context_menu(size_t index, Board *board);
+enum class ContextMenuAction {
+        None,
+        RenameBoard,
+        DeleteBoard,
+        DeleteEntry,
+        MoveEntry
+};
+static ContextMenuAction render_board_context_menu(size_t index, Board *board);
 static std::optional<Board> new_board_function();
 
 void gui_loop() {
     size_t index = 0;
     ImGui::SetNextWindowPos({20.0, 20.0});
+    bool break_the_loop = false;
     for (Board &board: BOARDS) {
         std::stringstream window_id;
         window_id << board.title;
         window_id << "###" << index;
         ImGui::Begin(window_id.str().c_str());
-            render_board_context_menu(index, &board);
+            auto action = render_board_context_menu(index, &board);
             render_board(index, &board);
             {
                 auto next_pos = ImGui::GetWindowPos();
                 next_pos.x += ImGui::GetWindowWidth() + 20;
                 ImGui::SetNextWindowPos(next_pos);
             }
+            switch(action) {
+                case ContextMenuAction::None:
+                    break;
+                case ContextMenuAction::DeleteBoard:
+                    ImGui::OpenPopup("###DELETE_BOARD");
+                    break;
+                case ContextMenuAction::RenameBoard:
+                    board.title_new = board.title;
+                    ImGui::OpenPopup("###RENAME_BOARD");
+                    break;
+                case ContextMenuAction::DeleteEntry:
+                    ImGui::OpenPopup("###DELETE_ENTRY");
+                    break;
+                case ContextMenuAction::MoveEntry:
+                    ImGui::OpenPopup("###MOVE_ENTRY");
+                    break;
+            }
+            if(ImGui::BeginPopup("###DELETE_BOARD")) {
+                ImGui::Text("Delete board");
+                ImGui::Separator();
+                ImGui::Text(board.title.c_str());
+                ImGui::Text("This action is irreversible");
+                if(ImGui::Button("Delete")) {
+                    BOARDS.erase(BOARDS.begin()+index);
+                    break_the_loop = true;
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+            if(ImGui::BeginPopup("###RENAME_BOARD")) {
+                ImGui::Text("Rename board");
+                ImGui::Separator();
+                if(ImGui::InputText("###BOARD_RENAME", &board.title_new, ImGuiInputTextFlags_EnterReturnsTrue)) {
+                    board.title = board.title_new;
+                    ImGui::CloseCurrentPopup();
+                }
+                if(ImGui::Button("Rename")) {
+                    board.title = board.title_new;
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::SameLine();
+                if(ImGui::Button("Cancel")) {
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+            if(ImGui::BeginPopup("###DELETE_ENTRY")) {
+                assert(board.hovered_index != SIZE_MAX);
+                ImGui::Text("Delete entry");
+                ImGui::Separator();
+                auto entry = board.items[board.hovered_index];
+                ImGui::Text(entry.text.c_str());
+                ImGui::Text("This action is irreversible");
+                if(ImGui::Button("Delete")) {
+                    board.items.erase(board.items.begin()+board.hovered_index);
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+            if(ImGui::BeginPopup("###MOVE_ENTRY")) {
+                auto entry_index = board.hovered_index;
+                assert(entry_index != SIZE_MAX);
+                ImGui::Text("Move entry");
+                ImGui::Separator();
+                size_t index = 0;
+                size_t selected_index = SIZE_MAX;
+                for(auto &board: BOARDS) {
+                    if(ImGui::MenuItem(board.title.c_str())) {
+                        selected_index = index;
+                        break;
+                    }
+                    index ++;
+                }
+                if(ImGui::MenuItem("Cancel")) {
+                    ImGui::CloseCurrentPopup();
+                }
+                if(selected_index != SIZE_MAX) {
+                    auto &from_board = board;
+                    auto &to_board = BOARDS[selected_index];
+                    auto entry = from_board.items[entry_index];
+                    from_board.items.erase(from_board.items.begin() + entry_index);
+                    to_board.items.push_back(entry);
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
         ImGui::End();
+        if(break_the_loop) break;
         index++;
     }
     if(auto board = new_board_function()) {
@@ -95,21 +202,37 @@ static void render_board(size_t board_index, Board *board) {
             }
         } else {
             ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(10.0, 10.0));
-            bool is_done_copy = entry.is_done;
-            ImGui::Checkbox("##CHECK_BOX", &is_done_copy);
-            ImGui::SameLine();
-            if(entry.is_done) {
-                ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(100, 100, 100, 255));
-                ImGui::TextWrapped(entry.text.c_str());
+                bool is_done_copy = entry.is_done;
+                ImGui::Checkbox("##CHECK_BOX", &is_done_copy);
+                // IsItemClicked() on the whole group will flip entry.is_done
+                // Here a temporary is created for Checkbox() to avoid double flipping
+
+                ImGui::SameLine();
+                ImU32 text_color = ImGui::GetColorU32(ImGuiCol_Text);
+                bool is_hovered = board->hovered_index == index;
+                if(entry.is_done) {
+                    text_color = is_hovered?
+                                    IM_COL32(100, 100, 150, 255):
+                                    IM_COL32(100, 100, 100, 255);
+                } else if(is_hovered) {
+                    text_color = IM_COL32(100, 100, 255, 255);
+                }
+                ImGui::PushStyleColor(ImGuiCol_Text, text_color);
+                    ImGui::TextWrapped(entry.text.c_str());
                 ImGui::PopStyleColor();
-            } else {
-                ImGui::TextWrapped(entry.text.c_str());
-            }
             ImGui::PopStyleVar();
             ImGui::Separator();
         }
         ImGui::EndGroup();
         ImGui::PopItemWidth();
+        if(ImGui::IsItemHovered()) {
+            board->hovered_index = index;
+        }
+        if(ImGui::IsWindowHovered() && 
+           board->hovered_index == index &&
+           !ImGui::IsItemHovered()) {
+            board->hovered_index = SIZE_MAX;
+        }
         if (ImGui::IsItemClicked()) {
             entry.is_done = !entry.is_done;
         }
@@ -121,18 +244,36 @@ static void render_board(size_t board_index, Board *board) {
     }
 }
 
-static void render_board_context_menu(size_t board_index, Board *board) {
-    if(ImGui::BeginPopupContextWindow("", 1, false)) {
-        ImGui::Text("Rename board: ");
-        if(ImGui::InputText("", &board->title, ImGuiInputTextFlags_EnterReturnsTrue)) {
+static ContextMenuAction render_board_context_menu(size_t board_index, Board *board) {
+    ContextMenuAction action = ContextMenuAction::None;
+    if(ImGui::BeginPopupContextWindow(NULL, 1, false)) {
+        if(board->hovered_index != SIZE_MAX) {
+            ImGui::Separator();
+            ImGui::Text("Entry");
+            ImGui::Separator();
+            if(ImGui::MenuItem("Delete")) {
+                action = ContextMenuAction::DeleteEntry;
+                ImGui::CloseCurrentPopup();
+            }
+            if(ImGui::MenuItem("Move")) {
+                action = ContextMenuAction::MoveEntry;
+                ImGui::CloseCurrentPopup();
+            }
+        }
+        ImGui::Separator();
+        ImGui::Text("Board");
+        ImGui::Separator();
+        if(ImGui::MenuItem("Rename")) {
+            action = ContextMenuAction::RenameBoard;
             ImGui::CloseCurrentPopup();
         }
-        if(ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape), false)) {
+        if(ImGui::MenuItem("Delete")) {
+            action = ContextMenuAction::DeleteBoard;
             ImGui::CloseCurrentPopup();
         }
-        ImGui::SetKeyboardFocusHere();
         ImGui::EndPopup();
     }
+    return action;
 }
 
 static std::optional<Board> new_board_function() {
