0a1ec56358c0a6392c152903bb7a9bea22369a1b
Farseen CK <itsfarseen@gmail.com>
2025-04-19 17:44:53 +0530

    fmt

--- models.hpp
+++ models.hpp
@@ -6,28 +6,27 @@
 #include <vector>
 
 struct Item {
-    std::string text;
-    bool is_done = false;
-    Item(std::string text) : text(text) {}
+  std::string text;
+  bool is_done = false;
+  Item(std::string text) : text(text) {}
 };
 
 struct Board {
-    std::string title;
-    std::vector<Item> items;
+  std::string title;
+  std::vector<Item> items;
 
-    //////////////////////////////////////
-    // UI Stuff 
-    //////////////////////////////////////
+  //////////////////////////////////////
+  // UI Stuff
+  //////////////////////////////////////
 
-    size_t editing_index = SIZE_MAX;
-    // Which item is being edited
+  size_t editing_index = SIZE_MAX;
+  // Which item is being edited
 
-    size_t hovered_index = SIZE_MAX;
-    // which item is being hovered
+  size_t hovered_index = SIZE_MAX;
+  // which item is being hovered
 
-    std::string title_new;
-    // Used as a buffer while renaming the board
+  std::string title_new;
+  // Used as a buffer while renaming the board
 };
-
 
 #endif // __MODELS_HPP__
